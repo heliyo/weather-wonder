1b2e9f953c014f53edf7f5f5669e269153b8ba73
heliyo <atefe.yn1@gmail.com>
2025-01-15 10:53:45 +0330

    added the basic Search Engine (no API)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function searchSubmit(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchForElement = document.querySelector("#search-form");
+searchForElement.addEventListener("submit", searchSubmit);
