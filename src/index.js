29e1698d552cc1757695b165a78bc1ac4aac8186
heliyo <atefe.yn1@gmail.com>
2025-01-18 18:54:18 +0330

    Inject the forecast html for JSC

--- src/index.js
+++ src/index.js
@@ -49,6 +49,32 @@ function searchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+  let forecastHtml = " ";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">⛅</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15°</strong>
+          </div>
+          <div class="weather-forecast-temperature">9°</div>
+        </div>
+      </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForElement = document.querySelector("#search-form");
 searchForElement.addEventListener("submit", searchSubmit);
+
 searchCity("paris");
+displayForecast();
